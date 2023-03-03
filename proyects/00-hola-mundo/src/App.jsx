737a179440b448bd696fc58f3971d1313a397147
Fabio Rojas <fabiorojas7@gmail.com>
2023-03-03 06:58:11 -0500

    hola mundo react

---
+++ proyects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+export const App = () => {
+  return (
+    <article>
+      <header>
+         <img src="https://unavatar.io/miduv" alt="" />
+      <div>
+        <strong>Fabio Antonio Rojas</strong>
+        <span>@hackchan77</span>
+      </div>
+      </header>
+      <aside>
+        <button>Seguir</button>
+      </aside>
+     
+    </article>
+  )
+}
+
